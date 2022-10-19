94b80e3c90234f57aac74afbef0107d1f038c24f
Isaiah411013 <Archangel_r320@hotmail.com>
2022-10-18 20:40:35 -0700

    updating questions & maarkdown

--- js/index.js
+++ js/index.js
@@ -1,82 +1,85 @@
+
 // packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const markDown = require('./utils/generateMarkdown.js')
+
 //questions for user input
-inquirer.prompt(
-    [
-        {
-            type: 'input',
-            message: 'What is the title of your project?',
-            name: 'title',
-            validate: (value) => { if (value) { return true } else { return 'Please enter a the name of your project.' } }
-        },
-        {
-            type: 'input',
-            message: 'Provide a short description.',
-            name: 'description',
-            validate: (value) => { if (value) { return true } else { return 'Please enter a description.' } }
-        },
-        {
-            type: 'input',
-            message: 'What are the steps required to install your project?',
-            name: 'installation',
-            validate: (value) => { if (value) { return true } else { return 'Please enter instalation method.' } }
-        },
-        {
-            type: 'input',
-            message: 'Provide instructions for use.',
-            name: 'usage',
-            validate: (value) => { if (value) { return true } else { return 'Please enter instalation method.' } }
-        },
-        {
-            type: 'input',
-            message: 'Are there any credits due?',
-            name: 'credits',
-            validate: (value) => { if (value) { return true } else { return 'Please add any credits needed.' } }
-        },
-        {
-            type: 'list',
-            message: ' What license did you use?',
-            name: 'license',
-            choices: ['The GPL License', 'The MIT License', 'GNU License', 'Apache License', 'N/A'],
-            validate: (value) => { if (value) { return true } else { return 'Please make a selection.' } }
-        },
+function userQuestions() {
 
-    ])
-    .then((data) => {
-        const layout =
-            `# ${title}
+    inquirer
+        .prompt(
+            [
+                {
+                    type: 'input',
+                    message: 'What is the title of your project?',
+                    name: 'title',
+                    validate: (value) => { if (value) { return true } else { return 'Please enter a the name of your project.' } }
+                },
+                {
+                    type: 'input',
+                    message: 'Provide a short description.',
+                    name: 'description',
+                    validate: (value) => { if (value) { return true } else { return 'Please enter a description.' } }
+                },
+                {
+                    type: 'input',
+                    message: 'What are the steps required to install your project?',
+                    name: 'installation',
+                    validate: (value) => { if (value) { return true } else { return 'Please enter instalation method.' } }
+                },
+                {
+                    type: 'input',
+                    message: 'Provide instructions for use.',
+                    name: 'usage',
+                    validate: (value) => { if (value) { return true } else { return 'Please enter instalation method.' } }
+                },
+                {
+                    type: 'input',
+                    message: 'Are there any credits due?',
+                    name: 'credits',
+                    validate: (value) => { if (value) { return true } else { return 'Please add any credits needed.' } }
+                },
+                {
+                    type: 'list',
+                    message: ' What license did you use?',
+                    name: 'license',
+                    choices: ['The GPL License', 'The MIT License', 'GNU License', 'Apache License', 'N/A'],
+                    validate: (value) => { if (value) { return true } else { return 'Please make a selection.' } }
+                },
+                {
+                    type: 'input',
+                    message: 'Is there a test?',
+                    name: 'test',
+                    validate: (value) => { if (value) { return true } else { return 'Please add any test criteria.' } }
+                },
+                {
+                    type: 'input',
+                    message: 'What is your GitHub user name?',
+                    name: 'githubuser',
+                    validate: (value) => { if (value) { return true } else { return 'Enter GitHub criteria.' } }
+                },
+                {
+                    type: 'input',
+                    message: 'Add your emmail address ',
+                    name: 'githubuser',
+                    validate: (value) => { if (value) { return true } else { return 'Whats your Email?' } }
+                },
 
-        ## Description
-            ${description}
 
-        ## Installation
-        ${installation}
-
-        ## Usage
-        ${usage}
-
-        ## Credits
-        ${credits}
-
-        ## License
-        ${license}`;
-
-        writeToFile(fileName, data);
-        // const filename = `${data.name.toLowerCase().split(' ').join('')}.md`;
-        // // write README file
-        // fs.writeFile(filename, layout, (err) =>
-        //     err ? console.log(err) : console.log('Success!')
-        // );
-    });
-
+            ]);
+}
 
 // // function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
 
-// // TODO: Create a function to initialize app
-// function init() { }
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('README.md has been generated!')
+    );
+}
 
-// // Function call to initialize app
-// init();
+// function to initialize app
+function init() { }
+
+//  call to initialize app
+init();
